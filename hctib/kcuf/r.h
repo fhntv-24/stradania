7cdb1d3b1d3c59f8c794bbaa3d425a5d4e7db6e7
fhntv-24 <1a2r3t4e5m6pylaev@gmail.com>
2024-12-12 01:38:45 +0300

    Add files via upload

---
+++ hctib/kcuf/r.h
@@ -0,0 +1,15 @@
+// struct and name  function
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef  struct magaz{
+        char t[100];
+        char p[100];
+        int cen;
+} magaz;
+void dob(magaz**ar,int*n);
+void viv(magaz**ar,int*n);
+void search(magaz**ar,int*n);
+void iz(magaz**ar,int*n);
+void ud(magaz**ar,int*n);
+
